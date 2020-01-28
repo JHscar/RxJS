0c7d1610b3463311dea6e50fbb2ca56109d3998d
JHscar <wogns5206@naver.com>
2020-01-28 09:46:50 +0900

    RxJS 배열과 비교해 본 옵저버블 연산자

---
+++ arrayOperator.js
@@ -0,0 +1,20 @@
+const { Observable } = require('rxjs');
+const { map } = require('rxjs/operators');
+
+const observableCreated$ = Observable.create(function (observer) {
+    observer.next(1);
+    observer.next(2);
+    observer.complete();
+});
+
+observableCreated$.pipe(
+    map(function (value) {
+        return value * 2;
+    })
+).subscribe(function next(item) {
+    console.log(item);
+})
+
+console.log([1, 2].map(function (value) {
+    return value * 2;
+}))
